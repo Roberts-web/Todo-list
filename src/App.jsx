fda10d5052861d4b569b5a2bf9e7047b2c3a55cb
robert ulicny <ulicny.robert@gmail.com>
2024-11-16 17:03:32 +0100

    click fixed on one click

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,13 @@ import TodoList from './components/TodoList';
 import InputList from './components/InputList';
 
 
-
-
 function App() {
-
 
   const [todoList,setTodoList] = useState([])
   const [newTask, setNewTask] = useState("")
   const [ color, setColor] = useState(true)
 
   
-
-
   const handleChange = (e) => {
     setNewTask(e.target.value)
   }
@@ -27,14 +22,11 @@ function App() {
       id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
       taskName: newTask,
       completed: false,
-      
-     
     }
     if( newTask.length > 0) {
       setTodoList([...todoList, task])
       setNewTask("")
     } else{return}
-    
   }
 
   // detele task
@@ -43,21 +35,16 @@ function App() {
    setTodoList(todoList.filter((task) => task.id !== id))
   }
 
-
  //completed task
-
-
-  
-
-
 
   const completeTask = (id) => {
     setTodoList(
       todoList.map((task) => {
-        if(task.id === id){
-          setColor(prevState => !prevState)
-          return {...task, completed: color }
-        } 
+        if(task.id === id && task.completed === false){
+          return {...task, completed: true}
+        } else if (task.id === id && task.completed === true){
+            return {...task, completed: false}
+          }
         else {
           return task
         }
@@ -66,43 +53,32 @@ function App() {
     )
   }
  
-
-
   return (
     <div className='App-header '>
       <div className='container'>
-          <InputList handleChange={handleChange}
+        <div>
+        <InputList handleChange={handleChange}
                      newTask={newTask} 
                      AddTask={AddTask}
-          />
-          
-          <div className='list'>
-            {todoList.map((task) => {
-            return <TodoList 
-                      taskName={task.taskName} 
-                      id={task.id}
-                      deleteAdd={deleteAdd}
-                      completeTask={completeTask}
-                      completed={task.completed}
-                      text={task.text}
-                      color={color}
-                    
-                        />
+        />
+        </div>
 
-            })}
-            
-          </div>
-
-
-
+        <div className='list'>
+          {todoList.map((task) => {
+          return <TodoList 
+                    taskName={task.taskName} 
+                    id={task.id}
+                    deleteAdd={deleteAdd}
+                    completeTask={completeTask}
+                    completed={task.completed}
+                    text={task.text}
+                    color={task.color}
+                      />
+          })}
+        </div>
       </div>
-    
-      
-
     </div>
-
   )
-  
 }
 
 export default App;
